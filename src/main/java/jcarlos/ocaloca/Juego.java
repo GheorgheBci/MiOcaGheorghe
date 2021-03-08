6579d10770692b0962b24a5c17dba9385071e656
Gheorghe <georgebucu96@gmail.com>
2021-03-08 11:17:18 +0100

    Añade comentarios al código y algunos pequeños cambios

--- src/main/java/jcarlos/ocaloca/Juego.java
+++ src/main/java/jcarlos/ocaloca/Juego.java
@@ -36,39 +36,77 @@ public class Juego {
         boolean salir = true;
         String boton;
 
-        ArrayList<String> jugadoresPozo = new ArrayList<>();
-
+        // Este bucle do while controla que el juego no pare hasta que algún jugador llegue a la casilla
+        // 63
         do {
+            // El for gestiona que jugador le toca jugar la ronda
             for (Jugador aux : cj.getTodosJugadores()) {
 
                 System.out.println("---------------------------------------------------");
 
+                // Si el jugador tiene turnos sin jugar entra en la primera condición del if
+                // donde se indica el nombre del jugador que no juega esa ronda y luego
+                // al total de turnosSinJugar se le restará uno 
                 if (aux.getTurnosSinJugar() > 0) {
                     System.out.println("El jugador " + aux.getNombre() + " no juega está ronda");
 
                     aux.setTurnosSinJugar(aux.getTurnosSinJugar() - 1);
 
+                    if (aux.getCasillaActual() == 31) {
+                        aux.tirarDado();
+
+                        Vista.informarTirada(aux);
+
+                        if (aux.getTirada() == 6) {
+                            System.out.println("El jugador " + aux.getNombre() + " ha salido del pozo");
+
+                            aux.setTurnosSinJugar(0);
+
+                            tablero.getCasilla(aux.getCasillaActual()).quitarJugador(aux);
+
+                            aux.mover(aux.getTirada());
+
+                            tablero.getCasilla(aux.getCasillaActual()).ponerJugador(aux);
+
+                            Vista.informarProgreso(aux);
+                            Vista.mostrarTablero(tablero);
+
+                        } else {
+                            System.out.println("El jugador " + aux.getNombre() + " no ha salido del pozo");
+                        }
+                    }
+
+                    // Si el jugador no tiene turnos sin jugar entra en el else
                 } else {
+                    // El jugador debe pulsar la tecla b para lanzar el dado
                     System.out.println("Pulsa la tecla b para tirar el dado");
                     boton = teclado.nextLine();
 
+                    // Este buble do while se repetirá mientras el jugador tenga otra tirada más
                     do {
 
                         if (boton.equalsIgnoreCase("b")) {
 
+                            // Primero quitamos el jugador de la casilla 1 
                             tablero.getCasilla(aux.getCasillaActual()).quitarJugador(aux);
 
+                            // El jugador tira el dado
                             aux.tirarDado();
 
+                            // Se moverá dependiendo del valor que ha sacado en el dado
                             aux.mover(aux.getTirada());
 
+                            // Y le colocamos en la casilla que le corresponde
                             tablero.getCasilla(aux.getCasillaActual()).ponerJugador(aux);
 
                             Vista.informarTirada(aux);
                             Vista.informarProgreso(aux);
                             Vista.mostrarTablero(tablero);
 
+                            // En el switch dado en la casilla que se encuentre el jugador entrará en el
+                            // case que le corresponde
                             switch (aux.getCasillaActual()) {
+                                // Corresponde con las casillas Oca 4
                                 case 5:
                                 case 14:
                                 case 23:
@@ -89,16 +127,20 @@ public class Juego {
 
                                     Vista.mostrarTablero(tablero);
 
+                                    // Si el jugador cae en la casilla 59 se mueve a la casilla 63, por tanto,
+                                    // dicho jugador ha ganado la partida
                                     if (aux.getCasillaActual() == 59) {
                                         salir = false;
 
                                         aux.setTiraOtraVez(false);
+                                    } else {
+                                        // Tiene otra tirada más
+                                        aux.setTiraOtraVez(true);
                                     }
-
-                                    aux.setTiraOtraVez(true);
 
                                     break;
 
+                                // Corresponde con las casillas Oca 5
                                 case 9:
                                 case 18:
                                 case 27:
@@ -122,6 +164,7 @@ public class Juego {
 
                                     break;
 
+                                // Corresponde con la casills Puente
                                 case 6:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla puente");
@@ -136,12 +179,13 @@ public class Juego {
 
                                     Vista.mostrarTablero(tablero);
 
-                                    aux.setTurnosSinJugar(1);
+                                    aux.setTurnosSinJugar(1); // El jugador pierde un turno
 
                                     aux.setTiraOtraVez(false);
 
                                     break;
 
+                                // Corresponde con la casilla Puente
                                 case 12:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla puente");
@@ -156,42 +200,37 @@ public class Juego {
 
                                     Vista.mostrarTablero(tablero);
 
-                                    aux.setTurnosSinJugar(1);
+                                    aux.setTurnosSinJugar(1); // El jugador pierde un turno
 
                                     aux.setTiraOtraVez(false);
 
                                     break;
 
+                                // Corresponde con la casilla Posada
                                 case 19:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla posada");
                                     System.out.println("Has perdido un turno");
 
-                                    aux.setTurnosSinJugar(1);
+                                    aux.setTurnosSinJugar(1); // El jugador pierde un turno
 
                                     aux.setTiraOtraVez(false);
 
                                     break;
 
+                                // Corresponde con la casilla Pozo
                                 case 31:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla pozo");
-                                    System.out.println("Hasta que no venga otro jugador no puedes jugar");
+                                    System.out.println("Hasta que no saques un 6 no puedes jugar");
 
                                     aux.setTurnosSinJugar(100);
-
-                                    jugadoresPozo.add(aux.getNombre());
-
-                                    if (jugadoresPozo.size() == 2) {
-                                        System.out.println("El jugador " + aux.getNombre() + " sale del pozo");
-                                        jugadoresPozo.remove(0);
-
-                                    }
 
                                     aux.setTiraOtraVez(false);
 
                                     break;
 
+                                // Corresponde con la casilla Laberinto
                                 case 42:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla laberinto");
@@ -199,7 +238,7 @@ public class Juego {
 
                                     tablero.getCasilla(aux.getCasillaActual()).quitarJugador(aux);
 
-                                    aux.mover(-12);
+                                    aux.mover(-12); // El jugador retrocede a la casilla 30
 
                                     tablero.getCasilla(aux.getCasillaActual()).ponerJugador(aux);
 
@@ -209,6 +248,7 @@ public class Juego {
 
                                     break;
 
+                                // Corresponde con la casilla Dado 26
                                 case 26:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla dado 26");
@@ -226,10 +266,11 @@ public class Juego {
 
                                     break;
 
+                                // Corresponde con la casilla Dado 53
                                 case 53:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla dado 53");
-                                    System.out.println("Por tanto avanza a la casilla " + (aux.getCasillaActual() - 53));
+                                    System.out.println("Por tanto avanza a la casilla " + ((63 + 10) - 53));
 
                                     tablero.getCasilla(aux.getCasillaActual()).quitarJugador(aux);
 
@@ -243,26 +284,27 @@ public class Juego {
 
                                     break;
 
+                                // Corresponde con la casilla Carcel
                                 case 56:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla carcel");
                                     System.out.println("Has perdido dos turnos");
 
-                                    aux.setTurnosSinJugar(2);
+                                    aux.setTurnosSinJugar(2); // Pierde 2 turnos
 
                                     aux.setTiraOtraVez(false);
 
                                     break;
 
+                                // Corresponde con la casilla Calavera
                                 case 58:
 
                                     System.out.println("El jugador " + aux.getNombre() + " ha llegado a la casilla calavera");
-                                    System.out.println("Por tanto retrocede a la casilla " + ((63 + 10) - 53));
-                                    System.out.println("Has perdido un turno");
+                                    System.out.println("Por tanto retrocede a la casilla " + (aux.getCasillaActual() - 57));
 
                                     tablero.getCasilla(aux.getCasillaActual()).quitarJugador(aux);
 
-                                    aux.mover(-57);
+                                    aux.mover(-57); // El jugador vuelve a la casilla 1
 
                                     tablero.getCasilla(aux.getCasillaActual()).ponerJugador(aux);
 
@@ -272,6 +314,7 @@ public class Juego {
 
                                     break;
 
+                                // Corresponde con las casillas normales
                                 default:
 
                                     aux.setTiraOtraVez(false);
@@ -283,6 +326,7 @@ public class Juego {
                     } while (aux.TiraOtraVez());
                 }
 
+                // Cuando un jugador llega a la casilla 63 entra en este if y la partida termina
                 if (aux.ganaPartida()) {
                     salir = false;
 
